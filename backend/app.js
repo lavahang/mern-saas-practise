efca23e064fbb1ffe9f6635c60c284ff97fe7565
lavahang <llavahang@gmail.com>
2025-10-03 23:14:43 +0545

    used get methods

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,15 @@
 let express =require('express') //import express from 'express'
 let app = express()
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello World22')
+})
+
+app.get('/about',(req,res)=>{
+  res.send('About Page')
+})
+
+app.listen(3000,()=>{
+    console.log('Server is running on http://localhost:3000')
+})
